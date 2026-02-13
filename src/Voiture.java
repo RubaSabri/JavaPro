2add404c6bb5b9fd2fff2db202fcb585951a7dc7
Ruba ALFATIH <rubaalfatih@MacBook-Air-de-Ruba.local>
2026-02-13 12:47:30 +0100

    commit changes

--- src/Voiture.java
+++ src/Voiture.java
@@ -1,4 +1,13 @@
 public class Voiture {
     String Oname ;
     int Vnumber ;
+
+    
+    
 }
+
+/*
+e compilateur crée le fichier .class parce que Java a besoin du bytecode compilé pour exécuter le programme. 
+il faut jamais le toucher 
+Le bytecode est un code intermédiaire entre le code source Java et le langage machine.
+ */
