eef15ac22e599d9a60c56f9b185771eeb5c2d6fb
Ruba ALFATIH <rubaalfatih@MacBook-Air-de-Ruba.local>
2026-02-13 12:36:16 +0100

    commit changes

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class App {    
+    public static void main(String[] args) throws Exception { 
+
+        System.out.println("Hello, World!"); 
         System.out.println("hello its Ruba");
+        Voiture voiture1 = new Voiture();  
+        voiture1.Oname = "Ruba";
+        voiture1.Vnumber = 123;
+        System.out.println(voiture1.Oname);
+        System.out.println(voiture1.Vnumber);
+
+
     }
 }
+
+/* 
+premer remarque il faut que le nom de ta classe soit la meme chose que le nom de ton fichier java
+
+ */
+
+
